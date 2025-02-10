aa16f7cf181f6cbfbec41d7ebf20d4af56fb2950
ananysagar <sagaranany577@gmail.com>
2025-02-10 22:34:00 +0530

    adding the Pagination component

---
+++ src/components/Pagination.js
@@ -0,0 +1,7 @@
+import React from "react";
+
+function Pagination() {
+  return <div>Hello Pagination</div>;
+}
+
+export default Pagination;
--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,18 +1,24 @@
 import React from "react";
 import "../styles.css";
+import Pagination from "./Pagination";
 function ProductCard({ data }) {
   return (
-    <div className="product-continer">
-      {data.map((product) => {
-        return (
+    <div>
+        <div className="pagination-container">
+            This is pagination
+        </div>
+      <div className="product-container">
+        {data.map((product) => {
+          return (
             <div className="card">
-                <div className="card-container">
-                    <img src={product.images[0]} alt={product.title}/>
-                    <h1 className="title">{product.title}</h1>
-                </div>
+              <div className="card-container">
+                <img src={product.images[0]} alt={product.title} />
+                <h1 className="title">{product.title}</h1>
+              </div>
             </div>
-        );
-      })}
+          );
+        })}
+      </div>
     </div>
   );
 }
