a7b692e0592e34134235c433f9604e636671b822
ananysagar <sagaranany577@gmail.com>
2025-02-23 23:47:09 +0530

    fixing pagination state in productCard

--- src/components/ProductCard.js
+++ src/components/ProductCard.js
@@ -1,18 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
 import "../styles.css";
 import Pagination from "./Pagination";
 function ProductCard({ data }) {
+  let [currentPage, setCurrentPage] = useState(1);
+  let productPerPage = 10;
+
+  let totalPages = Math.ceil(data.length / productPerPage);
+
+  let indexOfLastProduct = currentPage * productPerPage;
+  let indexOfFirstProduct = indexOfLastProduct - productPerPage;
+  const currentProducts = data.slice(indexOfFirstProduct, indexOfLastProduct);
+
+  function paginate(pageNumber) {
+    setCurrentPage(pageNumber);
+  }
+
   return (
     <div>
-        <div className="pagination-container">
-            This is pagination
-        </div>
+      <div className="pagination-container">
+        <Pagination
+          totalPages={totalPages}
+          paginate={paginate}
+          currentPage={currentPage}
+        />
+      </div>
       <div className="product-container">
-        {data.map((product) => {
+        {currentProducts.map((product) => {
           return (
             <div className="card">
               <div className="card-container">
-                <img src={product.images[0]} alt={product.title} />
+                <img src={product.thumbnail} alt={product.title} />
                 <h1 className="title">{product.title}</h1>
               </div>
             </div>
